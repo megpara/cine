0701a272308c10ddcd2d86471591c363220d34d1
Megan Paradowski <meganparadowski@Megans-MBP.socal.rr.com>
2024-04-15 20:21:13 -0700

    Insert banners, reels, and stills

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,31 @@ export default function Home() {
       target: targetRef,
     });
 
+    const [playCreator, setPlayCreator] = useState(false);
+    const [playDirector, setPlayDirector] = useState(false);
+    const [playCine, setPlayCine] = useState(false);
+    const [playEditor, setPlayEditor] = useState(false);
+
     const x = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
     useMotionValueEvent(scrollYProgress, "change", (latest) => {
+        if ( latest > 0.38 && latest < 0.39 ) {
+            setPlayDirector(true);
+        } else {
+            setPlayDirector(false);
+        }
+
+        if ( latest > 0.69 && latest < 0.7) {
+            setPlayCine(true);
+        } else {
+            setPlayCine(false);
+        }
+
+        if ( latest == 1 ) {
+            setPlayEditor(true);
+        } else {
+            setPlayEditor(false);
+        }
+
         console.log("Page scroll: ", latest);
     });
 
@@ -40,30 +63,69 @@ export default function Home() {
     }, [])
     return (
         <div className="container flex w-[532vw] max-w-[532vw] overflow-x-hidden overflow-y-auto">
-            <div className="panel w-[133vw] h-screen text-white relative">
+            
+            {/* CREATOR */}
+            <div className="panel w-[133vw] h-screen relative">
                 <div className="absolute top-0 left-0 w-full h-full flex">
-                    <div className="bg-red-500 w-[33vw] h-screen">Creator title</div>
-                    <div className="w-[100vw] h-screen text-white reel">Creator reel</div>
+                    <div className="bg-gray-500 text-white w-[33vw] h-screen flex flex-col items-center justify-center font-display">Creator Title</div>
+                    <div className="w-[100vw] h-screen text-white reel flex flex-col items-center justify-center font-display">Creator Reel</div>
                 </div>
             </div>
-            <div className="panel bg-white text-black w-[133vw] h-screen relative">
+
+            {/* DIRECTOR */}
+            <div className="panel w-[133vw] h-screen relative">
                 <div className="absolute top-0 left-0 w-full h-full flex">
-                    <div className="bg-red-500 w-[33vw] h-screen">Director title</div>
-                    <div className="w-[100vw] h-screen text-white reel">Director reel</div>
+                    {/* DIRECTOR TITLE */}
+                    <div className="w-[33vw] h-screen relative">
+                        <video className="absolute top-0 left-0 w-full h-full object-cover border-x-[5px] border-black" muted autoPlay loop src="/directing_banner.mp4" />
+                    </div>
+                    <div className="w-[100vw] h-screen reel relative">
+                        {/* DIRECTOR STILL */}
+                        <video className="absolute top-0 left-0 w-full h-full object-cover" muted autoPlay loop src="/directing_still.mp4" />
+                        {/* DIRECTOR REEL */}
+                        {playDirector && (
+                            <video className="absolute top-0 left-0 w-full h-full object-cover" muted autoPlay loop src="/directing_reel.mp4" />
+                        )}
+                    </div>
                 </div>
             </div>
-            <div className="panel w-[133vw] h-screen text-white relative">
+
+            {/* CINEMATOGRAPHER */}
+            <div className="panel w-[133vw] h-screen relative">
                 <div className="absolute top-0 left-0 w-full h-full flex">
-                    <div className="bg-red-500 w-[33vw] h-screen">Cinematographer title</div>
-                    <div className="w-[100vw] h-screen text-white reel">Cinematographer reel</div>
+                    {/* CINEMATOGRAPHER TITLE */}
+                    <div className="w-[33vw] h-screen relative">
+                        <video className="absolute top-0 left-0 w-full h-full object-cover border-x-[5px] border-black" muted autoPlay loop src="/cine_banner.mp4" />
+                    </div>
+                    <div className="w-[100vw] h-screen reel relative">
+                        {/* CINEMATOGRAPHER STILL */}
+                        <video className="absolute top-0 left-0 w-full h-full object-cover" muted autoPlay loop src="/cine_still.mp4" />
+                        {/* CINEMATOGRAPHER REEL */}
+                        {playCine && (
+                            <video className="absolute top-0 left-0 w-full h-full object-cover" muted autoPlay loop src="/cine_reel.mp4" />
+                        )}
+                    </div>
                 </div>
             </div>
-            <div className="panel w-[133vw] h-screen bg-white text-black relative">
+
+            {/* EDITOR */}
+            <div className="panel w-[133vw] h-screen relative">
                 <div className="absolute top-0 left-0 w-full h-full flex">
-                    <div className="bg-red-500 w-[33vw] h-screen">Editor title</div>
-                    <div className="w-[100vw] h-screen text-white reel">Editor reel</div>
+                    {/* EDITOR TITLE */}
+                    <div className="w-[33vw] h-screen relative">
+                        <video className="absolute top-0 left-0 w-full h-full object-cover border-x-[5px] border-black" muted autoPlay loop src="/editing_banner.mp4" />
+                    </div>
+                    <div className="w-[100vw] h-screen reel relative">
+                        {/* EDITOR STILL */}
+                        <video className="absolute top-0 left-0 w-full h-full object-cover" muted autoPlay loop src="/editing_still.mp4" />
+                        {/* EDITOR REEL */}
+                        {playEditor && (
+                            <video className="absolute top-0 left-0 w-full h-full object-cover" muted autoPlay loop src="/editing_reel.mp4" />
+                        )}
+                    </div>
                 </div>
             </div>
+    
         </div>
     )
 }
