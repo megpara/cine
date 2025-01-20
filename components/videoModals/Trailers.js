83a0c3c7a422709a9909a471f7fe9f979cb1fde1
megpara <meganparadowski@Megans-MBP.socal.rr.com>
2025-01-20 13:51:22 -0800

    Multiple trailers on Trailer page

--- components/videoModals/Trailers.js
+++ components/videoModals/Trailers.js
@@ -38,12 +38,12 @@ export default function Trailers({ open, toggle }) {
           >
             </motion.div>
 
-            <div className="content flex flex-col items-center justify-center z-60 w-screen h-screen">
+            <div className="content flex flex-col items-center justify-center z-60 w-screen h-full overflow-scroll">
                 <motion.div 
                     initial={{ opacity: 0, y: 50 }}
                     animate={{ opacity: 1, y: 0 }}
                     exit={{ opacity: 0, y: 50, transition: { duration: 0.5 } }}
-                    className="w-3/4 md:w-1/2">
+                    className="w-3/4 md:w-1/2 py-24 h-full">
                     <div className="relative w-full pt-[56.25%] pb-12 px-12" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                         <iframe
                             src="https://www.youtube.com/embed/I0JKALT6Ry4?si=Aa1kyHY6f8vSvuc_"
@@ -53,10 +53,37 @@ export default function Trailers({ open, toggle }) {
                             className="absolute top-0 left-0 w-full h-full"
                         ></iframe>
                     </div>
-                    <div className="text-sm font-display uppercase leading-8 text-center pt-12">
+                    <div className="text-sm font-display uppercase leading-8 text-center py-6">
                         <div className="text-lg">Destination NBA</div>
                         Production Company: Religion of Sports <br />
                         Directors: Liam Hughes & Bryant Robinson <br />
+                        Trailer Editor: Colin Lupe
+                    </div>
+                    <div className="relative w-full pt-[56.25%] pb-6 px-12" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                    <iframe
+                            src="https://player.vimeo.com/video/913122773?h=9eb257f6fa&badge=0&autopause=0&player_id=0&app_id=58479&title=0&portrait=0&byline=0"
+                            frameBorder="0"
+                            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+                            title="DAM"
+                            className="absolute top-0 left-0 w-full h-full"
+                        ></iframe>
+                    </div>
+                    <div className="text-sm font-display uppercase leading-8 text-center">
+                        <div className="text-lg">Heist: The Money Plane</div>
+                    </div>
+                    <div className="relative w-full pt-[56.25%] pb-6 px-12" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                    <iframe
+                            src="https://player.vimeo.com/video/913120730?h=2163830fbd&badge=0&autopause=0&player_id=0&app_id=58479&title=0&portrait=0&byline=0"
+                            frameBorder="0"
+                            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+                            title="DAM"
+                            className="absolute top-0 left-0 w-full h-full"
+                        ></iframe>
+                    </div>
+                    <div className="text-sm font-display uppercase leading-8 text-center pb-12">
+                        <div className="text-lg pb-6">Heist: The Bourbon King</div>
+                        Production Company: Dirty Robber <br />
+                        Creators: Derek Dohen and Martin Roe <br />
                         Trailer Editor: Colin Lupe
                     </div>
                 </motion.div>
