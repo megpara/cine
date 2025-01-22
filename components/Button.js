107537070dff7b806745327b6df6aa6e2bd76d2a
megpara <meganparadowski@Megans-MBP.socal.rr.com>
2025-01-21 17:11:19 -0800

    Change scroll button position to fixed for mobile

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,6 @@
 export default function Button({ children }) {
     return (
-        <div className="absolute w-screen h-[60px] bottom-0 z-30 flex flex-col items-center justify-center">
+        <div className="fixed w-screen h-[60px] bottom-0 z-30 flex flex-col items-center justify-center">
         <div className="py-2 px-4 bg-[#2700f9] font-display text-sm tracking-wide rounded-full">
             {children}
         </div>
